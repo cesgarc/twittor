56a73a86443a831764fe6e1635f810dac6f413aa
Cesar Garcia <cesgarc@gmail.com>
2018-12-11 17:32:57 +0000

    Primer Commit

---
+++ js/sw-utils.js
@@ -0,0 +1,13 @@
+//Archivo auxiliar del SW
+
+function actualizaCacheDinamico( dynamicCache, req, res ) {
+    if ( res.ok){
+        return caches.open ( dynamicCache).then( cache => {
+            cache.put ( req, res.clone());
+            return res.clone();
+        });
+    }
+    else{
+        return res;
+    }
+}
